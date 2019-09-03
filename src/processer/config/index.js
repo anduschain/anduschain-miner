90ae6d5be1e0a036c3f8e316ca1d7015f215d7f1
hakuna <kwangmin010@gmail.com>
2019-09-03 10:41:24 +0900

    fix : binary url

--- src/processer/config/index.js
+++ src/processer/config/index.js
@@ -25,7 +25,7 @@ const logPath = () => {
 
 const DefaultSetting = {
     LOG_PATH : logPath(),
-    BINARY_URL : 'https://raw.githubusercontent.com/anduschain/andusChainGethBinary/deb/nodeBinary.json',
+    BINARY_URL : 'https://raw.githubusercontent.com/anduschain/anduschain-miner/master/src/processer/nodeBinary.json',
 };
 
 const nodeDataDir = () => {
